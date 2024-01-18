cfe64c1b720ba77b1cc1f6b6753dbd30af6fc485
DESKTOP-IRVF2MB\Admin <PQT280203@outlook.com>
2024-01-18 22:01:08 +0700

    [THANG] [CREATE PROPERTYDTO,USERDETAILDTO]

---
+++ TPSS.Data/Models/DTO/UserDetailDTO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPSS.Data.Models.Entities;
+
+namespace TPSS.Data.Models.DTO
+{
+    public class UserDetailDTO
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? PersonalId { get; set; }
+
+        public string? Avatar { get; set; }
+
+        public string UserId { get; set; } = null!;
+
+        public DateOnly? DateOfBirth { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Gender { get; set; }
+
+        public DateOnly? CreateDate { get; set; }
+
+        public DateOnly? UpdateDate { get; set; }
+
+        public string? CreateBy { get; set; }
+
+        public string? UpdateBy { get; set; }
+
+        public string? TaxIdentificationNumber { get; set; }
+
+        
+    }
+}
