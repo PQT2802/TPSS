4d7ea5aaf6279dd555d9f16d2ef5306f6387cb22
DESKTOP-E6UFM4J\hung <offunikeynt@gmail.com>
2024-02-07 17:59:59 +0700

    Merge branch 'DEV_THANG' into DEV
    # Conflicts:
    #	TPSS.Business/Service/Impl/UserService.cs
    #	TPSS.Business/TPSS.Business.csproj
    #	TPSS.Data/Helper/BaseRepository.cs
    #	TPSS.Data/Models/DTO/UserDTO.cs
    #	TPSS.Data/Repository/Impl/UserRepository.cs
    #	Test_1/Helper/DependencyInjectionHelper.cs
    #	Test_1/Program.cs
    #	Test_1/appsettings.json

---
+++ TPSS.Data/Models/DTO/LoginDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPSS.Data.Models.DTO
+{
+    public class LoginDTO
+    {
+        public string Password { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        //public string UsernameOrEmailOrPhone { get; set; } = null!;
+        
+    }
+}
